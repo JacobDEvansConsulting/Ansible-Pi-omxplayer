46fd27f94bc9525aa16c19a63dd184ed137432e6
Jacob Evans <git@jacobdevans.com>
2016-01-22 23:04:29 -0500

    raspi-config update

--- files/raspi-init.sh
+++ files/raspi-init.sh
@@ -1,17 +1,12 @@
-# Set locale to en_US.UTF-8
+# backup Files
 cp /etc/locale.gen /etc/locale.gen.dist
-sed -i -e "/^[^#]/s/^/#/" -e "/en_US.UTF-8/s/^#//" /etc/locale.gen
-update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
-# Actually, don't do dpkg-reconfigure, it brings back en_GB.UTF-8 and mixes it
-# into the locales in a weird way.
-# dpkg-reconfigure -f noninteractive locales
-
-# Set timezone to America/New_York
 cp /etc/timezone /etc/timezone.dist
-echo "America/New_York" > /etc/timezone
-dpkg-reconfigure -f noninteractive tzdata
-
-# Set the keyboard to US, don't set any modifier keys, etc.
 cp /etc/default/keyboard /etc/default/keyboard.dist
+#Update Configs
+sed -i -e "/^[^#]/s/^/#/" -e "/en_US.UTF-8/s/^#//" /etc/locale.gen
 sed -i -e "/XKBLAYOUT=/s/gb/us/" /etc/default/keyboard
+echo "America/New_York" > /etc/timezone
+#Set new configs
+update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+dpkg-reconfigure -f noninteractive tzdata
 service keyboard-setup restart
